fc16eafce3e981ae41ed423b3a8f459c0982dd1e
Thiago Tescaro <tbnas@outlook.com>
2018-12-11 17:45:50 -0200

    Teste exercicio 1

---
+++ udemy/python_e_kivy/exercicio1-1.py
@@ -0,0 +1,4 @@
+#Faça um algoritmo que apenas imprima o seu nome na tela e em seguida finalize a aplicação.
+
+print("Thiago Braga Nasscimento Tescaro")
+# A função Print() permite imprimir na tela os valores que os objetos representam
